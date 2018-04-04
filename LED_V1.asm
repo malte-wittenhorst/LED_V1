3603d177de3158002ac4594f2a3dfa6f575ed60a
Malte Wittenhorst <malte.wittenhorst@rwth-aachen.de>
2018-04-04 18:02:32 +0200

    code

--- LED_V1.asm
+++ LED_V1.asm
@@ -7,51 +7,65 @@
     processor 16f1827
 
 ; Pinbelegung:
-; RA0 NC/Ausgang
-; RA1 NC/Ausgang
+; RA0 SPI out/NC
+; RA1 Test LED/NC
 ; RA2 NC/Ausgang
-; RA3 LED rot
-; RA4 LED grün
-; RA5 Encoder 1 rechts
-; RA6 SPI out
-; RA7 LED blau
+; RA3 LED Pin 3
+; RA4 LED Pin 4
+; RA5 Encoder 3 links
+; RA6 NC/Ausgang
+; RA7 LED Pin 2
 ; RB0 SPI Interrupt
-; RB1 SPI in
-; RB2 Encoder 2 rechts
-; RB3 Encoder 3 rechts
-; RB4 SPI clock
-; RB5 Encoder 1 links 
-; RB6 Encoder 2 links
-; RB7 Encoder 3 links
+; RB1 Encoder 1 links
+; RB2 SPI in/NC
+; RB3 Encoder 1 rechts
+; RB4 Encoder 2 links
+; RB5 SPI clock/NC 
+; RB6 Encoder 2 rechts
+; RB7 Encoder 3 rechts
 
+    #define DEBUG_LED LATA,1
 TRIS_ROT   equ TRISA
-TRI_GRUEN  equ TRISA
+TRIS_GRUEN  equ TRISA
     #define TRIS_BIT_ROT  TRISA,3
     #define TRIS_BIT_GRUEN TRISA,4
 
 OSCCON_init equ B'00111010'  ; 500 kHz internal oscillator
 OPTION_REG_init equ B'00001000'
 TRISA_init equ B'00100000'
-TRISB_init equ B'11101111'
+TRISB_init equ B'11011111'
 WPUB_init  equ 0xFF
 IOCBP_init equ B'00000000'
-IOCBN_init equ B'11100000'
+IOCBN_init equ B'11001000'
 ROT_init    equ 0x80
-GRUEN_init  equ	0x80
+GRUEN_init  equ	0x10
 BLAU_init   equ 0x80
 CCPTMRS_init equ B'00000000'
 PR2_PWM_init equ 0xFF
 CCP3CON_init equ B'00001100'
+CCP4CON_init equ B'00001100'
+T2CON_init equ B'00000100'
+INTCON_init equ B'00001000' ;Enable IOC
 
 
-	; __config _CONFIG1 _FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF  & _CLKOUTEN_OFF & _IESO_OFF & FCMEN_OFF
-	; __config _CONFIG2 _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_HI & _LVP_OFF
+	 __config _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF  & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
+	 __config _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_HI & _LVP_OFF
 	
 load_reg macro Reg, value ; load register with predefined value
 	banksel Reg
 	movlw   value
 	movwf   Reg
 	endm
+	
+debug_led macro an_aus
+	banksel  LATA
+	if an_aus == 1
+	    bsf  DEBUG_LED
+	else
+	    bcf  DEBUG_LED
+	endif
+	endm
+	    
 
 ;*****************************************
 ; Memory
@@ -68,7 +82,8 @@ BLAU	equ	22
 
 
 start	
-	bsf	INTCON,GIE
+	;bsf	INTCON,GIE
+	debug_led 1
 loop	goto	loop ; Endlosschleife	
 
 
@@ -87,7 +102,8 @@ init    load_reg OSCCON, OSCCON_init
 	load_reg GRUEN, GRUEN_init
 	load_reg BLAU, BLAU_init
 	load_reg CCPTMRS, CCPTMRS_init
-
+	    
+	
 	banksel  TRIS_ROT
 	bsf	 TRIS_BIT_ROT
 	load_reg PR2, PR2_PWM_init
@@ -96,8 +112,22 @@ init    load_reg OSCCON, OSCCON_init
    	movf	 ROT,w
 	banksel  CCPR3L
 	movwf    CCPR3L
+	load_reg T2CON, T2CON_init
+	banksel  TRIS_ROT
+	bcf	 TRIS_BIT_ROT
 	
-	goto start
+	banksel  TRIS_GRUEN
+	bsf	 TRIS_BIT_GRUEN
+	load_reg CCP4CON, CCP4CON_init
+	banksel  GRUEN
+   	movf	 GRUEN,w
+	banksel  CCPR4L
+	movwf    CCPR4L
+	banksel  TRIS_GRUEN
+	bcf	 TRIS_BIT_GRUEN
+	
+	load_reg  INTCON,INTCON_init
+	goto	 start
 ;*****************************************	
 	end
 
