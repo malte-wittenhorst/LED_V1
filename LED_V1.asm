0e8b11cf22a7b075de45213acdd4757689fdb59e
Malte Wittenhorst <malte.wittenhorst@rwth-aachen.de>
2018-04-06 11:29:32 +0200

    Alle Encoder aktiv

--- LED_V1.asm
+++ LED_V1.asm
@@ -34,8 +34,10 @@
     #define DEBUG_LED LATA,1
 TRIS_ROT   equ TRISA
 TRIS_GRUEN  equ TRISA
+TRIS_BLAU  equ TRISA
     #define TRIS_BIT_ROT  TRISA,3
     #define TRIS_BIT_GRUEN TRISA,4
+    #define TRIS_BIT_BLAU TRISA,7
 
 OSCCON_init equ B'00111010'  ; 500 kHz internal oscillator
 OPTION_REG_init equ B'00001000'
@@ -45,12 +47,14 @@ WPUB_init  equ 0xFF
 IOCBP_init equ B'00000000'
 IOCBN_init equ B'11001000'
 ROT_init    equ 0x80
-GRUEN_init  equ	0x01
-BLAU_init   equ 0x80
+GRUEN_init  equ	0x00
+BLAU_init   equ 0x40
 CCPTMRS_init equ B'00000000'
 PR2_PWM_init equ 0xFF
+CCP2CON_init equ B'00001100'
 CCP3CON_init equ B'00001100'
 CCP4CON_init equ B'00001100'
+PSTR2CON_init equ B'00000001'
 T2CON_init equ B'00000100'
 T4CON_init equ B'00000001' ;Prescaler 4
 PR4_init equ .64
@@ -99,6 +103,14 @@ ENC1_C  equ	2
 ENC_EN equ      0024 ;Encoder enable
  R1    equ      0 ;Rechts Encoder 1
  L1    equ      1 ;Links Encoder 1
+ R2    equ	2
+ L2    equ      3
+ R3    equ      4
+ L3    equ      5
+HUE_LOW equ	0025
+HUE_HIGH equ    0026
+SAT	equ	0027
+VAL	equ	0028
  
 
 ;*****************************************
@@ -141,10 +153,15 @@ enc1rechts
 	
 	debug_led_toggle
 	
-	banksel GRUEN
+	banksel VAL
+	movlw   .16
+	addwf   VAL,f
+	btfsc   STATUS,C
+	call    v_setmax
 	
-	movlw   10
-	addwf   GRUEN,w
+	movf    VAL,w
+	banksel GRUEN
+	movwf   GRUEN
 	movf    GRUEN,w
 	banksel CCPR4L
 	movwf   CCPR4L
@@ -158,10 +175,114 @@ enc1links
 	bcf     ENC_EN,R1
 	bcf     ENC_EN,L1
 	
+	banksel VAL
+	movlw   .16
+	subwf   VAL,f
+	btfss   STATUS,C
+	clrf    VAL
+	movf    VAL,w
 	banksel GRUEN
+	movwf   GRUEN
+	movf    GRUEN,w
+	banksel CCPR4L
+	movwf   CCPR4L
 	
-	movlw   10
-	subwf   GRUEN,f
+	goto    timer4start
+	
+enc2int ;debug_led 1
+	bcf     IOCBF,ENC2
+	movlb   PORT_BANK
+	btfss   ENC2_L
+	goto    enc2rechts
+	goto    enc2links
+
+enc2rechts
+	banksel ENC_EN
+	btfss   ENC_EN,R2
+	goto    timer4start
+	bcf     ENC_EN,R2
+	bcf     ENC_EN,L2
+	
+	banksel VAL
+	movlw   .16
+	addwf   VAL,f
+	btfsc   STATUS,C
+	call    v_setmax
+	
+	movf    VAL,w
+	banksel GRUEN
+	movwf   GRUEN
+	movf    GRUEN,w
+	banksel CCPR4L
+	movwf   CCPR4L
+	
+	goto    timer4start
+	
+enc2links
+	banksel ENC_EN
+	btfss   ENC_EN,L2
+	goto    timer4start
+	bcf     ENC_EN,R2
+	bcf     ENC_EN,L2
+	
+	banksel VAL
+	movlw   .16
+	subwf   VAL,f
+	btfss   STATUS,C
+	clrf    VAL
+	movf    VAL,w
+	banksel GRUEN
+	movwf   GRUEN
+	movf    GRUEN,w
+	banksel CCPR4L
+	movwf   CCPR4L
+	
+	goto    timer4start
+	
+enc3int ;debug_led 1
+	bcf     IOCBF,ENC3
+	movlb   PORT_BANK
+	btfss   ENC3_L
+	goto    enc3rechts
+	goto    enc3links
+
+enc3rechts
+	banksel ENC_EN
+	btfss   ENC_EN,R3
+	goto    timer4start
+	bcf     ENC_EN,R3
+	bcf     ENC_EN,L3
+	
+	banksel VAL
+	movlw   .16
+	addwf   VAL,f
+	btfsc   STATUS,C
+	call    v_setmax
+	
+	movf    VAL,w
+	banksel GRUEN
+	movwf   GRUEN
+	movf    GRUEN,w
+	banksel CCPR4L
+	movwf   CCPR4L
+	
+	goto    timer4start
+	
+enc3links
+	banksel ENC_EN
+	btfss   ENC_EN,L3
+	goto    timer4start
+	bcf     ENC_EN,R3
+	bcf     ENC_EN,L3
+	
+	banksel VAL
+	movlw   .16
+	subwf   VAL,f
+	btfss   STATUS,C
+	clrf    VAL
+	movf    VAL,w
+	banksel GRUEN
+	movwf   GRUEN
 	movf    GRUEN,w
 	banksel CCPR4L
 	movwf   CCPR4L
@@ -175,27 +296,11 @@ timer4start           ;start timer 4
 	banksel TMR4
 	clrf    TMR4
 	return
-
-enc2int movlb   PORT_BANK
-	btfss   ENC2_L
-	goto    enc2links
-	goto    enc2rechts
-enc2links
-	;debug_led 1
-	return
-enc2rechts
-	;debug_led 0
-	return
 	
-enc3int movlb   PORT_BANK
-	btfss   ENC3_L
-	goto    enc3links
-	goto    enc3rechts
-enc3links
-	;debug_led 1
-	return
-enc3rechts
-	;debug_led 0
+v_setmax
+	banksel VAL
+	movlw   0xFF
+	movwf   VAL
 	return
 	
 tmr4_int 
@@ -205,6 +310,10 @@ tmr4_int
 	clrf    TMR4
 	banksel PIR3
 	bcf     PIR3,TMR4IF ;clear interrupt flag
+	call    en_enc1
+	call    en_enc2
+	call    en_enc3
+	return
         
 en_enc1 movlb   PORT_BANK
 	btfss   ENC1_L
@@ -221,6 +330,36 @@ en_enc1_l ;Enable left
 	bsf     ENC_EN,L1	
 	return
 
+en_enc2 movlb   PORT_BANK
+	btfss   ENC2_L
+	goto    en_enc2_r
+	goto    en_enc2_l
+	
+en_enc2_r ;Enable right
+	banksel ENC_EN
+	bsf     ENC_EN,R2
+	return
+	
+en_enc2_l ;Enable left
+	banksel ENC_EN
+	bsf     ENC_EN,L2	
+	return
+	
+en_enc3 movlb   PORT_BANK
+	btfss   ENC3_L
+	goto    en_enc3_r
+	goto    en_enc3_l
+	
+en_enc3_r ;Enable right
+	banksel ENC_EN
+	bsf     ENC_EN,R3
+	return
+	
+en_enc3_l ;Enable left
+	banksel ENC_EN
+	bsf     ENC_EN,L3	
+	return
+	
 start	bsf     INTCON,PEIE
 	bsf	INTCON,GIE
 	;debug_led 1
@@ -267,10 +406,36 @@ init    load_reg OSCCON, OSCCON_init
 	banksel  TRIS_GRUEN
 	bcf	 TRIS_BIT_GRUEN
 	
+	banksel  TRIS_GRUEN
+	bsf	 TRIS_BIT_GRUEN
+	load_reg CCP4CON, CCP4CON_init
+	banksel  GRUEN
+   	movf	 GRUEN,w
+	banksel  CCPR4L
+	movwf    CCPR4L
+	banksel  TRIS_GRUEN
+	bcf	 TRIS_BIT_GRUEN
+	
+	banksel  TRIS_BLAU
+	bsf	 TRIS_BIT_BLAU
+	load_reg PSTR2CON,PSTR2CON_init
+	load_reg CCP2CON, CCP2CON_init
+	banksel  BLAU
+   	movf	 BLAU,w
+	banksel  CCPR2L
+	movwf    CCPR2L
+	banksel  TRIS_BLAU
+	bcf	 TRIS_BIT_BLAU
+	
 	load_reg INTCON,INTCON_init
 	load_reg T4CON,T4CON_init
 	load_reg PR4,PR4_init
 	load_reg PIE3,PIE3_init
+	
+	load_reg HUE_LOW,0
+	load_reg HUE_HIGH,0
+	load_reg VAL,0
+	load_reg SAT,0
 	
 	pagesel  start
 	goto	 start
