e97cae4f609bf318b4783ff89253371954885d95
Malte Wittenhorst <malte.wittenhorst@rwth-aachen.de>
2018-04-06 17:42:09 +0200

    Alles implementiert

--- LED_V1.asm
+++ LED_V1.asm
@@ -10,11 +10,11 @@
 ; RA0 SPI out/NC
 ; RA1 Test LED/NC
 ; RA2 NC/Ausgang
-; RA3 LED Pin 3
-; RA4 LED Pin 4
+; RA3 LED Pin 3   Rot
+; RA4 LED Pin 4   Grün
 ; RA5 Encoder 3 links
 ; RA6 NC/Ausgang
-; RA7 LED Pin 2
+; RA7 LED Pin 2   Blau
 ; RB0 SPI Interrupt
 ; RB1 Encoder 1 links
 ; RB2 SPI in/NC
@@ -61,6 +61,10 @@ PR4_init equ .64
 INTCON_init equ B'00001000' ;Enable IOC
 PIE3_init equ B'00000010' ;Enable TMR4 Interrupt
 ENC_EN_init equ 0xFF
+HUE_LOW_init equ 00
+HUE_HIGH_init equ 00
+VAL_init equ .255
+SAT_init equ .255
 
 
 	 __config _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF  & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
@@ -91,26 +95,32 @@ debug_led_toggle macro
 ;*****************************************
 ; Memory
 ; Bank0:
-ROT	equ     0020
-GRUEN	equ	0021
-BLAU	equ	0022
-ENCSTAT equ     0023
+ROT	equ     0x0020
+GRUEN	equ	0x0021
+BLAU	equ	0x0022
+ENCSTAT equ     0x0023
 ENC1_AKT equ	0
 ENC1_DIR equ    1
 ENC1_C  equ	2
  ;Bit 0 Encoder 1 aktiv
  ;Bit 1 Encoder 1 Richtung, 0 rechts, 1 links
-ENC_EN equ      0024 ;Encoder enable
+ENC_EN equ      0x0024 ;Encoder enable
  R1    equ      0 ;Rechts Encoder 1
  L1    equ      1 ;Links Encoder 1
  R2    equ	2
  L2    equ      3
  R3    equ      4
  L3    equ      5
-HUE_LOW equ	0025
-HUE_HIGH equ    0026
-SAT	equ	0027
-VAL	equ	0028
+HUE_LOW equ	0x0025
+HUE_HIGH equ    0x0026
+SAT	equ	0x0027
+VAL	equ	0x0028
+AARGB0  equ	0x0029
+AARGB1  equ	0x002A
+BARGB0	equ     0x002B
+RGB_p	equ	0x002C
+RGB_q	equ	0x002D
+RGB_t	equ	0x002E
  
 
 ;*****************************************
@@ -125,6 +135,16 @@ VAL	equ	0028
 	btfsc   PIR3,TMR4IF
 	call    tmr4_int
 	retfie
+	
+get_rot brw
+	dt	VAL,RGB_q,RGB_p,RGB_p,RGB_t,VAL
+
+get_gruen brw
+	dt	RGB_t,VAL,VAL,RGB_q,RGB_p,RGB_p
+
+get_blau brw
+	dt	RGB_p,RGB_p,RGB_t,VAL,VAL,RGB_q
+	
 ioc_int 
 	;debug_led 1
 	banksel IOCBF ;ioc interrupt
@@ -156,15 +176,12 @@ enc1rechts
 	banksel VAL
 	movlw   .16
 	addwf   VAL,f
+	movlw   0xFF
 	btfsc   STATUS,C
-	call    v_setmax
+	movwf   VAL
 	
-	movf    VAL,w
-	banksel GRUEN
-	movwf   GRUEN
-	movf    GRUEN,w
-	banksel CCPR4L
-	movwf   CCPR4L
+	pagesel compute_rgb
+	call	compute_rgb
 	
 	goto    timer4start
 	
@@ -175,17 +192,16 @@ enc1links
 	bcf     ENC_EN,R1
 	bcf     ENC_EN,L1
 	
+	debug_led 0
+	
 	banksel VAL
 	movlw   .16
 	subwf   VAL,f
 	btfss   STATUS,C
 	clrf    VAL
-	movf    VAL,w
-	banksel GRUEN
-	movwf   GRUEN
-	movf    GRUEN,w
-	banksel CCPR4L
-	movwf   CCPR4L
+
+	pagesel compute_rgb
+	call    compute_rgb
 	
 	goto    timer4start
 	
@@ -209,8 +225,10 @@ enc2rechts
 	movlw   0xFF
 	btfsc   STATUS,C
 	movwf   SAT
-	goto    timer4start
-	incf    VAL,f
+	
+	pagesel compute_rgb
+	call    compute_rgb
+	
 	goto    timer4start
 	
 enc2links
@@ -224,7 +242,11 @@ enc2links
 	movlw   .16
 	subwf   SAT,f
 	btfss   STATUS,C
-	clrf    SAT 
+	clrf    SAT
+	
+	pagesel compute_rgb
+	call    compute_rgb	
+	
 	goto    timer4start
 	
 enc3int ;debug_led 1
@@ -241,18 +263,16 @@ enc3rechts
 	bcf     ENC_EN,R3
 	bcf     ENC_EN,L3
 	
+	debug_led 1
+	
 	banksel HUE_LOW
 	movlw   .16
-	addwf   VAL,f
+	addwf   HUE_LOW,f
 	btfsc   STATUS,C
-	call    v_setmax
+	call    inc_hue
 	
-	movf    VAL,w
-	banksel GRUEN
-	movwf   GRUEN
-	movf    GRUEN,w
-	banksel CCPR4L
-	movwf   CCPR4L
+	pagesel compute_rgb
+	call    compute_rgb	
 	
 	goto    timer4start
 	
@@ -263,19 +283,37 @@ enc3links
 	bcf     ENC_EN,R3
 	bcf     ENC_EN,L3
 	
-	banksel VAL
+	debug_led 0
+	
+	banksel HUE_LOW
 	movlw   .16
-	subwf   VAL,f
+	subwf   HUE_LOW,f
 	btfss   STATUS,C
-	clrf    VAL
-	movf    VAL,w
-	banksel GRUEN
-	movwf   GRUEN
-	movf    GRUEN,w
-	banksel CCPR4L
-	movwf   CCPR4L
+	call    dec_hue
+
+	pagesel compute_rgb
+	call    compute_rgb
 	
 	goto    timer4start
+	
+inc_hue 
+	banksel HUE_HIGH
+	incf    HUE_HIGH,f
+	movlw   .6
+	xorwf   HUE_HIGH,w
+	btfsc   STATUS,Z
+	clrf    HUE_HIGH
+	return
+
+dec_hue
+	banksel HUE_HIGH
+	decf    HUE_HIGH,f
+	comf    HUE_HIGH,w
+	btfss   STATUS,Z
+	return
+	movlw   .5
+	movwf   HUE_HIGH
+	return
 	
 timer4start           ;start timer 4
 	banksel T4CON
@@ -284,13 +322,7 @@ timer4start           ;start timer 4
 	banksel TMR4
 	clrf    TMR4
 	return
-	
-v_setmax
-	banksel VAL
-	movlw   0xFF
-	movwf   VAL
-	return
-	
+
 tmr4_int 
 	banksel T4CON
 	bcf     T4CON,TMR4ON ;stop timer4
@@ -347,6 +379,87 @@ en_enc3_l ;Enable left
 	banksel ENC_EN
 	bsf     ENC_EN,L3	
 	return
+
+compute_rgb
+	banksel AARGB0
+	comf	SAT,w
+	movwf   AARGB0
+	incf    AARGB0,f
+	movf    VAL,w
+	movwf   BARGB0
+	call    FXM0808U
+	movf    AARGB1,w
+	movwf   RGB_p
+	movf    SAT,w
+	movwf   AARGB0
+	movf    HUE_LOW,w
+	movwf   BARGB0
+	call    FXM0808U
+	comf    AARGB1,w
+	movwf   AARGB0
+	incf    AARGB0,f
+	movf    VAL,w
+	movwf   BARGB0
+	call    FXM0808U
+	movf    AARGB1,w
+	movwf   RGB_q
+	comf    HUE_LOW,w
+	movwf   AARGB0
+	incf    AARGB0,f
+	movf    SAT,w
+	movwf   BARGB0
+	call    FXM0808U
+	comf    AARGB1,w
+	movwf   AARGB0
+	incf    AARGB0,f
+	movf    VAL,w
+	movwf   BARGB0
+	call    FXM0808U
+	movf    AARGB1,w
+	movwf   RGB_t
+	
+	pagesel get_rot
+	movf    HUE_HIGH,w
+	call    get_rot
+	movwf   FSR0L
+	movlw   high RGB_p
+	movwf   FSR0H
+	movf    INDF0,w
+	movwf   ROT
+	
+	pagesel get_gruen
+	movf    HUE_HIGH,w
+	call    get_gruen
+	movwf   FSR0L
+	movlw   high RGB_p
+	movwf   FSR0H
+	movf    INDF0,w
+	movwf   GRUEN
+	
+	pagesel get_blau
+	movf    HUE_HIGH,w
+	call    get_blau
+	movwf   FSR0L
+	movlw   high RGB_p
+	movwf   FSR0H
+	movf    INDF0,w
+	movwf   BLAU
+	
+	movf    ROT,w
+	banksel CCPR3L
+	movwf   CCPR3L
+	
+	movf    GRUEN,w
+	banksel CCPR4L
+	movwf   CCPR4L
+	
+	movf    BLAU,w
+	banksel CCPR2L
+	movwf   CCPR2L
+	
+	return
+	
+	
 	
 start	bsf     INTCON,PEIE
 	bsf	INTCON,GIE
@@ -365,9 +478,14 @@ init    load_reg OSCCON, OSCCON_init
 	load_reg WPUB, WPUB_init
 	load_reg IOCBP, IOCBP_init
 	load_reg IOCBN, IOCBN_init
-	load_reg ROT, ROT_init
-	load_reg GRUEN, GRUEN_init
-	load_reg BLAU, BLAU_init
+	;load_reg ROT, ROT_init
+	;load_reg GRUEN, GRUEN_init
+	;load_reg BLAU, BLAU_init
+	load_reg HUE_LOW,HUE_LOW_init
+	load_reg HUE_HIGH,HUE_HIGH_init
+	load_reg VAL,VAL_init
+	load_reg SAT,SAT_init
+	call	compute_rgb ;init rgb
 	load_reg CCPTMRS, CCPTMRS_init
 	load_reg ENC_EN,ENC_EN_init
 	    
@@ -428,6 +546,53 @@ init    load_reg OSCCON, OSCCON_init
 	pagesel  start
 	goto	 start
 ;*****************************************	
+	
+;**********************************************************************************************
+UMUL0808 macro
+; Max Timing: 2+5+7*4 = 35 clks
+; Min Timing: 2+16+3 = 21 clks
+; PM: 2+2*8+4+7*4 = 50 DM: 3
+ variable i = 0
+     BCF STATUS,C ; clear carry for first right shift
+    MOVF AARGB0,W
+
+ while i < 8
+
+    BTFSC BARGB0,i
+    GOTO UM0808NA#v(i)
+    variable i = i + 1
+ endw
+    CLRF AARGB0 ; if we get here, BARG = 0
+    RETURN
+UM0808NA0 RRF AARGB0, F
+    RRF AARGB1, F
+    variable i = 1
+ while i < 8
+    BTFSC BARGB0,i
+    ADDWF AARGB0, F
+UM0808NA#v(i) RRF AARGB0, F
+     RRF AARGB1, F
+    variable i = i + 1
+ endw
+     endm
+;**********************************************************************************************
+
+; 8x8 Bit Unsigned Fixed Point Multiply 8x8 -> 16
+; Input: 8 bit unsigned fixed point multiplicand in AARGB0
+; 8 bit unsigned fixed point multiplier in BARGB0
+; Use: CALL FXM0808U
+; Output: 8 bit unsigned fixed point product in AARGB0
+; Result: AARG <-- AARG x BARG
+; Max Timing: 1+70+2 = 73 clks
+; Min Timing: 1+53 = 54 clks
+; PM: 1+19+1 = 21 DM: 4
+FXM0808U
+        banksel  AARGB1
+        CLRF AARGB1 ; clear partial product
+	UMUL0808
+	RETLW 0x00
+;**********************************************************************************************
+;**********************************************************************************************	
 	end
 
  
