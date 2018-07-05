165ca8cbc3221ad001f83635aa54b97d8b48cea4
Malte Wittenhorst <malte.wittenhorst@rwth-aachen.de>
2018-07-05 10:49:41 +0200

    Code Encoder

--- ENCODER.asm
+++ ENCODER.asm
@@ -1,6 +1,8 @@
 
 
-ENC_PORT equ
+ENC1_PORT equ
+ENC2_PORT equ
+ENC3_PORT equ
 ENC1_L equ
 ENC2_L equ
 ENC3_L equ
@@ -14,13 +16,34 @@ ENC	udata
 
 
 enc_int	macro num_enc ; num_enc specifies encoder
-	banksel ENC_PORT
-	btfsc ENC#v(num_enc)_L    ;check if pushed or released
-        goto  enc_push#v(num_enc) ;
+	banksel ENC#v(num_enc)_PORT
+	btfsc ENC#v(num_enc)_L      ;check if pushed or released
+        goto  enc_push#v(num_enc)   ;
 	banksel ENC
 	btfss ENC#v(num_enc)_pushed ; released, check if pushed
-	retfie                    ; if not, do nothing
-	btfss ENC_TIMER_ON
-	call  debounce_timer_start
-        debounce_timer_reset
+	retfie                      ; if not, do nothing
+	btfss ENC_TIMER_ON          ; transition pushed->released
+	call  debounce_timer_start  ; start the timer, if not already started
+        debounce_timer_reset        ; reset timer
+	
+	endm
 		
+enc_push macro num_enc  ; ENC bank is already selected
+	btfsc ENC#v(num_enc)_pushed ; check if already pushed
+	retfie                      ; if so, do nothing
+	bsf   ENC#v(num_enc)_pushed ; else, set pushed flag
+	banksel ENC#v(num_enc)_PORT
+	btfss ENC#v(num_enc)_R      ; check direction
+	goto  enc#v(num_enc)_left 
+	goto  enc#v(num_enc)_right  
+	endm	
+
+enc_left macro
+
+	retfie
+	endm
+
+enc_right macro
+
+	retfie
+	endm
